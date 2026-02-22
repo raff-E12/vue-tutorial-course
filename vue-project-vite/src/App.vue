cb4e41e0178a31d29b7801a275ffc42d413c12e7
raff-E12 <raffaelecoppola381@gmail.com>
2026-02-22 21:59:10 +0100

    feat: Modellazione della pagina...

--- vue-project-vite/src/App.vue
+++ vue-project-vite/src/App.vue
@@ -1,30 +1,98 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { BApp, BNavbar, BButton } from "bootstrap-vue-next"
+import CountsTable from "./components/CountsTable.vue";
+import { ref } from "vue";
+
+const isShow = ref(false);
+const count = ref(0);
+
+const HandleCountAdd = () => {
+   count.value++;
+}
+
+const HandleShowCount = () => {
+  isShow.value = !isShow.value;
+}
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+   <BApp class="app-bt">
+    <main class="main-sc">
+     <BNavbar
+         class="navbar"
+         fixed="top"
+        >
+          <BNavbarBrand
+            tag="h1"
+            class="navbrand-sc"
+            >Vue <b class="text-success">CLI</b>
+            </BNavbarBrand>
+      </BNavbar>
+
+      <div class="container-sc">
+        <h2>Benvenuto in Vue.js</h2>
+
+        <section class="section-components">
+          <BButton variant="success" @click="HandleShowCount">Mostra Contatore</BButton>
+           <CountsTable title="Contatore A" text="Clicca qui!!" v-on:click-count="HandleCountAdd" :count="count" :show="isShow" />
+        </section>
+      </div>
+
+    </main>
+   </BApp>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+@import url('https://fonts.googleapis.com/css2?family=Gabarito:wght@400..900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&family=Quicksand:wght@300..700&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap');
+
+:root{
+  --font-montserrant: "Montserrat", sans-serif;
+  --font-nunito: "Nunito Sans", sans-serif;
+  --bg-success: #198754;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+.main-sc{
+  width: 100%;
+  height: 100dvh;
+  background-color: #f8f9fa;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.app-bt{
+  width: auto;
+  height: auto;
+}
+
+.navbar{
+  background-color: #004a28;
+  padding: 16px;
+  border-bottom: 5px solid #198754;
+}
+
+.navbar .navbrand-sc{
+  font-family: var(--font-montserrant);
+  color: #ffff;
+  font-size: 1.13em;
+  font-weight: 700;
+}
+
+.container-sc{
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 2px;
+  padding-top: 6em;
+}
+
+.section-components{
+  margin-top: 20px;
+  padding: 3px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 2px;
 }
 </style>
---
+++ vue-project-vite/src/components/CountsTable.vue
@@ -0,0 +1,89 @@
+<script setup lang="ts">
+import { BApp, BNavbar, BButton } from "bootstrap-vue-next"
+import { ref } from "vue";
+
+const props = defineProps({
+  title: { type: String, required: true }, 
+  text: { 
+   type: String, 
+   required: true,
+   validator(value, props) {
+      // Uso del Validatore per seguenti valori di props
+      return ['Clicca qui!!', 'Clicca', 'Aumenta'].includes(value)
+    } 
+  },
+  count: { type: Number, required: true },
+  show: Boolean
+});
+
+const emits = defineEmits(['click-count']);
+
+// Con uso della funzione setup() senza la funzione di
+// "setup" in script
+
+// export default {
+   // props: [ 'title', 'text', 'count', 'show'],
+   // emits: ['click-count'],
+   // setup(props, ctx){
+      // const HandleClick = () => {
+         // console.log("click bottone")
+         // ctx.emit('click-count')
+      // }
+
+      // return {
+         // HandleClick
+      // }
+   // }
+//}
+
+</script>
+
+<template>
+   <main class="count-sc shadow" v-if="show">
+      <div class="display-count-sc">
+         <h3>{{ props.title }}</h3>
+         <p class="count-num">{{ count }}</p>
+      </div>
+      <div class="cotainer-btns">
+         <BButton @click="emits('click-count')">{{ props.text }}</BButton>
+      </div>
+   </main>
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Gabarito:wght@400..900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&family=Quicksand:wght@300..700&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap');
+
+:root{
+  --font-montserrant: "Montserrat", sans-serif;
+  --font-nunito: "Nunito Sans", sans-serif;
+  --bg-success: #198754;
+}
+
+ .count-sc{
+   width: 400px;
+   background: #c8c8c8;
+   border-radius: 20px;
+   padding: 16px 17px;
+   text-align: center;
+   color: #fff;
+   margin-top: 1em;
+ }
+
+ .count-num{
+    background: #6c6c6c;
+    width: 60px;
+    padding: 12px;
+    border-radius: 12px;
+    font-family: monospace;
+    text-align: center;
+    font-weight: 800;
+ }
+
+ .display-count-sc{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 12px;
+ }
+</style>
